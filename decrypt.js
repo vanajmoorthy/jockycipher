b6a1c58afeb810217067f8fba1b3c9543d5a8f3b
vanajmoorthy <vanajmoorthy@gmail.com>
2019-03-27 22:06:31 +0530

    Initial commit

---
+++ decrypt.js
@@ -0,0 +1,39 @@
+function mencrypt() {
+  var nameInput = document.getElementById("dec").value;
+  var name = nameInput.split(" ");
+
+  var newArr = [];
+  var finalArr = [];
+  var vals = "Aa";
+  var valueOfUpper = vals.charCodeAt(0);
+  var valueOfLower = vals.charCodeAt(1);
+  // var newString;
+
+  for (var i = 0; i < name.length; i++)
+    for (var j = 0; j < name[i].length; j++) {
+
+      var word = name[i];
+      var charNeed = word.charAt(j);
+      var charNeedUpper = charNeed.toUpperCase();
+
+      if (charNeed == charNeedUpper) {
+        var codeNum = ((name[i].charCodeAt(j) - valueOfUpper) - name.length) % 26;
+        var newCodeNum = codeNum + valueOfUpper;
+        var res = String.fromCharCode(newCodeNum);
+        newArr[i] = res;
+      }
+      else {
+        var codeNum = ((name[i].charCodeAt(j) - valueOfLower) - name.length) % 26;
+        var newCodeNum = codeNum + valueOfLower;
+        var res = String.fromCharCode(newCodeNum);
+        var newString = "";
+        newString += res;
+      }
+      finalArr[j] = newString;
+    }
+  finalString = finalArr.join("");
+  console.log(finalString);
+  // console.log(newArr);
+  // console.log(finalString);
+  document.getElementById("gOutput").value = finalString;
+}
---
+++ encrypt.js
@@ -0,0 +1,41 @@
+function encrypt() {
+  var name = document.getElementById("name").value;
+  var nameArray = name.split(" ");
+
+  var valz = "Aa";
+  var valueOfA = valz.charCodeAt(0);
+  var valueOfa = valz.charCodeAt(1);
+
+  var newArr = [];
+  var finalArr = [];
+  var newString = "";
+  for (var j = 0; j < nameArray.length; j++) {
+    var str = nameArray[j];
+    var n = str.length;
+
+    for (var i = 0; i < n; i++) {
+      var charNeeded = str.charAt(i);
+      var charNeededUpper = charNeeded.toUpperCase();
+
+      if (charNeeded == charNeededUpper) {
+        var codeNum = ((str.charCodeAt(i) - valueOfA) + n) % 26;
+        var newCodeNum = codeNum + valueOfA;
+        var res = String.fromCharCode(newCodeNum);
+        newArr[i] = res;
+
+      } else {
+        var codeNum = ((str.charCodeAt(i) - valueOfa) + n) % 26;
+        var newCodeNum = codeNum + valueOfa;
+        var res = String.fromCharCode(newCodeNum);
+        newArr[i] = res;
+      }
+      newString = newArr.join("")
+      finalArr[j] = newString;
+    }
+  }
+
+  finalString = finalArr.join(" ");
+  // console.log(newArr);
+  console.log(finalString);
+  document.getElementById("output").value = finalString;
+}
