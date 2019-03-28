9185f7adcf1a66d79ff07cfaf2f47fbeb9524b17
vanajmoorthy <vanajmoorthy@gmail.com>
2019-03-28 11:35:34 +0530

    Final for now

--- encrypt.js
+++ encrypt.js
@@ -1,41 +1,61 @@
-function encrypt() {
-  var name = document.getElementById("name").value;
-  var nameArray = name.split(" ");
-
-  var valz = "Aa";
-  var valueOfA = valz.charCodeAt(0);
-  var valueOfa = valz.charCodeAt(1);
-
-  var newArr = [];
-  var finalArr = [];
-  var newString = "";
-  for (var j = 0; j < nameArray.length; j++) {
-    var str = nameArray[j];
-    var n = str.length;
-
-    for (var i = 0; i < n; i++) {
-      var charNeeded = str.charAt(i);
-      var charNeededUpper = charNeeded.toUpperCase();
-
-      if (charNeeded == charNeededUpper) {
-        var codeNum = ((str.charCodeAt(i) - valueOfA) + n) % 26;
-        var newCodeNum = codeNum + valueOfA;
-        var res = String.fromCharCode(newCodeNum);
-        newArr[i] = res;
-
-      } else {
-        var codeNum = ((str.charCodeAt(i) - valueOfa) + n) % 26;
-        var newCodeNum = codeNum + valueOfa;
-        var res = String.fromCharCode(newCodeNum);
-        newArr[i] = res;
-      }
-      newString = newArr.join("")
-      finalArr[j] = newString;
+function encrypt(forward) {
+    // target input (either decryption or encryption)
+    if (forward) {
+      var input = document.getElementById("encryptionInput").value;
+    } else {
+      var input = document.getElementById("decryptionInput").value;
     }
-  }
+    var output = '';
+    var sentence = input.split(' ');
 
-  finalString = finalArr.join(" ");
-  // console.log(newArr);
-  // console.log(finalString);
-  document.getElementById("output").value = finalString;
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    // for each word in the sentence
+    for (var i = 0; i < sentence.length; i++) {
+      var word = sentence[i];
+      // this is the amount by which each letter is shifted
+      var n = word.length;
+      // for decryption
+      if (!forward) {
+        n *= -1;
+      }
+
+      // for each letter in the word
+      for (var j = 0; j < word.length; j++) {
+        var letter = word[j];
+
+        // check if letter is uppercase (this is preserved)
+        var upper = false;
+        if (letter == letter.toUpperCase()) {
+          upper = true;
+        }
+
+        letter = letter.toLowerCase();
+        var index = alphabet.indexOf(letter);
+        // if index is negative, character is not a letter
+        if (index >= 0) {
+          // shift forward by n, mod 26 to create a loop (add 26 to prevent negative numbers)
+          index = (index + n + 26) % 26;
+
+          var outputChar = alphabet[index];
+          if (upper) {
+            outputChar = outputChar.toUpperCase();
+          }
+        } else {
+          var outputChar = letter;
+        }
+        output = output + outputChar;
+      }
+      // end of word
+      output = output + " ";
+    }
+    // target output (either decryption or encryption)
+    if (forward) {
+      document.getElementById("encryptionOutput").innerHTML = output;
+    } else {
+      document.getElementById("decryptionOutput").innerHTML = output;
+      // alert(output);
+      // console.log(word.length);
+      // document.getElementById(decryptionOutput).size = word.length;
+    }
 }
